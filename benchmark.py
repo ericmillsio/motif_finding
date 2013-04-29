b66c9c8b4c190e321e11d5c1b5c83bb87a22ed1f
Eric Mills <ericmills2@gmail.com>
2013-04-28 19:13:47 -0500

    everything is in place for benchmark, need to unnormalize motif now

--- benchmark.py
+++ benchmark.py
@@ -17,11 +17,39 @@ def main(argv):
       sys.exit(2)
 
    seqs = [gen_seq(sl) for i in range(sc)]
-   print seqs
+   # print seqs
    motif = gen_motif(icpc, ml)
-   print motif
-   sample_motif(motif)
+   # print motif
+   samples = [sample_motif(motif) for i in range(sc)]
+
+   seqs = [plant(seq, samp) for samp, seq in  zip(samples, seqs)]
+
+   with open('sequences.fa', 'w') as f:
+      for i, s in enumerate(seqs):
+         f.write(">seq" + str(i) + "\n" + ''.join(s[0]) + "\n")
+
+   with open('sites.txt', 'w') as f:
+      for i, s in enumerate(seqs):
+         f.write("seq" + str(i) + "\t" + str(s[1]) + "\n")
+
+   with open('motif.txt', 'w') as f:
+      f.write(">MOTIF1" + "\t" + str(ml) + "\n")
+      for i, s in enumerate(motif):
+         for x in s[0]:
+            f.write(str(x) + "\t")
+         f.write("\n")
+
+   with open('motiflength.txt', 'w') as f:
+      f.write(str(ml))
+
    sys.exit()
+
+
+def plant(seq, site):
+   pos = random.randint(0, len(seq)-len(site)-1)
+   for i, seed in enumerate(site):
+      seq[pos+i] = seed
+   return (seq, pos)
 
 
 def gen_motif(icpc, ml):
@@ -30,7 +58,7 @@ def gen_motif(icpc, ml):
       found_one = False
       while not found_one:
          # generate random row
-         col = np.random.uniform(100,size=(1,4))
+         col = np.random.uniform(100000000,size=(1,4))
          # normalize
          col /= col.sum()
          ic, within_rng = info_content(col, 0.25, icpc)
@@ -42,15 +70,17 @@ def gen_motif(icpc, ml):
 def sample_motif(motif):
    sample = []
    for col in motif:
-      poss = random.random()
-      curr = 0
-      for i, letter in enumerate(col):
-         print letter
-         if poss <= letter:
-            sample.append(nuc)
-            break;
-         curr += letter
-   print sample
+      sample.append( select_from_distrib(col[0].tolist()) )
+   return sample
+
+def select_from_distrib(distrib):
+   poss = random.random()
+   curr = 0
+   for i, letter in enumerate(distrib):
+      curr += letter
+      if poss <= curr:
+         return nuc[i]
+   return nuc[3]
 
 def info_content(col, bg, icpc):
    ic = 0
@@ -59,9 +89,9 @@ def info_content(col, bg, icpc):
    return ic, icpc-0.2 <= ic <= icpc+0.2
       
 def gen_seq(sl):
-   seq = ""
+   seq = []
    for x in range(sl):
-      seq += nuc[random.randint(0, 3)]
+      seq.append(nuc[random.randint(0, 3)])
    return seq
 
 if __name__ == "__main__":
